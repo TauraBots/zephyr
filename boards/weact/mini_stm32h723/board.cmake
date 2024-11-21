942c06791f57d64052538dfffb5bd806f7d4e4a4
Thassio <thxssio@gmail.com>
2024-11-21 17:02:05 -0300

    draft: Update Zephyr (Quebrei na hora de subir :{{{{{)

--- boards/weact/mini_stm32h723/board.cmake
+++ boards/weact/mini_stm32h723/board.cmake
@@ -1,9 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
-# Ajustando para o STM32H723VGT6
+# Configurações específicas para o STM32H723VGT6
 board_runner_args(dfu-util "--pid=0483:df11" "--alt=0" "--dfuse")
 board_runner_args(jlink "--device=STM32H723VGT6" "--speed=4000")
 
-# Incluindo os scripts de runner para dfu-util e JLink
+# Incluir os scripts de runner para dfu-util e JLink
 include(${ZEPHYR_BASE}/boards/common/dfu-util.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
